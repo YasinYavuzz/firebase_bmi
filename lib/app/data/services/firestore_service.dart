09e8e9d8762ceda3ddd6e821af5222cd526d6a69
YasinYavuzz <yavuzyasin.eem@gmail.com>
2023-01-02 20:02:13 +0300

    sa

--- lib/app/data/services/firestore_service.dart
+++ lib/app/data/services/firestore_service.dart
@@ -1,12 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_bmi/app/data/services/firebase_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class FirestoreService extends GetxService{
   Future<FirestoreService> init() async{
     return this;
   } 
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
   addUsers(email, Map<String, dynamic> data){
     FirebaseFirestore.instance.collection('Users').doc(email).set(data);
+  }
+
+  addBMI(Map<String,dynamic> data){
+    FirebaseFirestore.instance.collection('Users').doc(auth.currentUser!.email).collection('BMI').add(data);
   }
 }
--- lib/app/ui/pages/home_page/home_page.dart
+++ lib/app/ui/pages/home_page/home_page.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 import '../../../controllers/home_controller.dart';
 
 
@@ -10,17 +11,134 @@ class HomePage extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       //backgroundColor: Colors.amber,
-      drawer: Drawer(
-        child: Icon(Icons.double_arrow_sharp),
-        backgroundColor: Colors.black,
-      ),
+      
       appBar: AppBar(
-        systemOverlayStyle: const SystemUiOverlayStyle(statusBarColor: Colors.black),
+        systemOverlayStyle: const SystemUiOverlayStyle(statusBarColor: Colors.amber),
         elevation: 0,
         toolbarHeight: 0,
       ),
-      body: Column(
-        
+      body: Container(
+        width: 100.w,
+        height: 100.h,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.amber, Colors.white])
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                //alignment: Alignment.center,
+                width: 100.w,
+                height: 30.h,
+                //color: Colors.red,
+                child: Card(
+                  color: const Color.fromARGB(255, 59, 49, 49),
+                  elevation: 10,
+                  shadowColor: Colors.grey,
+                  child: Column(
+                    children: [
+                      Flexible(
+                        flex: 5, child: Container(decoration: const BoxDecoration(image: DecorationImage(image: AssetImage('assets/home/im_bmi.png'))),),
+                      ),
+                      Flexible(
+                        flex: 1, child: Container(color: Colors.red,child: const Center(child: Text('Vücut Kitle Indeksi Hesaplama',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),)),),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                //alignment: Alignment.center,
+                width: 100.w,
+                height: 30.h,
+                //color: Colors.red,
+                child: Card(
+                  color: const Color.fromARGB(255, 59, 49, 49),
+                  elevation: 10,
+                  shadowColor: Colors.grey,
+                  child: Column(
+                    children: [
+                      Flexible(
+                        flex: 5, child: Container(decoration: const BoxDecoration(image: DecorationImage(image: AssetImage('assets/home/im_graph.png'))),),
+                      ),
+                      Flexible(
+                        flex: 1, child: Container(color: Colors.red,child: const Center(child: Text('Kilo Takip Çizelgesi',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),)),),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                //alignment: Alignment.center,
+                width: 100.w,
+                height: 30.h,
+                //color: Colors.red,
+                child: Card(
+                  color: const Color.fromARGB(255, 59, 49, 49),
+                  elevation: 10,
+                  shadowColor: Colors.grey,
+                  child: Column(
+                    children: [
+                      Flexible(
+                        flex: 5, child: Container(decoration: const BoxDecoration(image: DecorationImage(image: AssetImage('assets/home/im_nitrution_schedule.png'))),),
+                      ),
+                      Flexible(
+                        flex: 1, child: Container(color: Colors.red,child: const Center(child: Text('Beslenme Programı',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),)),),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                //alignment: Alignment.center,
+                width: 100.w,
+                height: 30.h,
+                //color: Colors.red,
+                child: Card(
+                  color: const Color.fromARGB(255, 59, 49, 49),
+                  elevation: 10,
+                  shadowColor: Colors.grey,
+                  child: Column(
+                    children: [
+                      Flexible(
+                        flex: 5, child: Container(decoration: const BoxDecoration(image: DecorationImage(image: AssetImage('assets/home/im_excercise.png'))),),
+                      ),
+                      Flexible(
+                        flex: 1, child: Container(color: Colors.red,child: const Center(child: Text('Antreman Programı',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),)),),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                //alignment: Alignment.center,
+                width: 100.w,
+                height: 30.h,
+                //color: Colors.red,
+                child: Card(
+                  color: const Color.fromARGB(255, 59, 49, 49),
+                  elevation: 10,
+                  shadowColor: Colors.grey,
+                  child: Column(
+                    children: [
+                      Flexible(
+                        flex: 5, child: Container(decoration: const BoxDecoration(image: DecorationImage(image: AssetImage('assets/home/im_fasting.png'))),),
+                      ),
+                      Flexible(
+                        flex: 1, child: Container(color: Colors.red,child: const Center(child: Text('Aralıklı Oruç Programı',style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),)),),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
